8e80fc212e68094aad7325e7fe3ce2e963e105c8
Derek Reimanis <derek.reimanis@gmail.com>
2023-09-01 10:48:26 -0600

    struggling with json encoding, not sure why json.dumps can't export a json root object. Maybe its from me coming at this from a primarily java background, not a pyhon background. either way, very close

--- cwe-view-converters/main.py
+++ cwe-view-converters/main.py
@@ -2,53 +2,20 @@ import sys
 import re
 import argparse
 import csv
-from bs4 import BeautifulSoup
+import json
 from pathlib import Path
 
 
-def importXML(filename):
-    with open(filename, encoding="utf-8") as f:
-        return BeautifulSoup(f.read(), "xml")
-
-
-def getWeaknessFromID(cwe_id):
-    weakness = weaknesses.find("Weakness", {"ID": cwe_id})
-    return weakness
-
-
 def parse_xml(input_file):
-    print("parse xml")
-    exit()
-    data = importXML(input_file)
-    global views
-    views = data.find_all('Views')
-    global weaknesses
-    weaknesses = data.find('Weaknesses')
-    # more often than not, the input xml file will have only 1 view, but I am generalizing it just in case...
-    for view in views:
-        for member in view.find_all('Members'):
-            for has_member in member.find_all('Has_Member'):
-                cwe_id = has_member['CWE_ID']
-                weakness = getWeaknessFromID(cwe_id)
-                name = weakness['Name']
-                description = weakness.find('Description')
-                positive = False
-                for related_weakness in weakness.find_all('Related_Weaknesses'):
-                    print(related_weakness.find['CWE_ID'])
-                parents = ""
-
-                weaknesses.update({cwe_id: MeasureNode(name, description, positive, parents)})
-
-                # print(description)
+    print("Not yet implemented")
+    exit(0)
 
 
-class MeasureNode:
-    def __init__(self, cwe_id, name, weakness_abstraction, description, children_ids):
-        self.cwe_id = cwe_id
-        self.name = name
-        self.weakness_abstraction = weakness_abstraction
-        self.description = description
-        self.children_ids = children_ids
+def export_to_json(output_file, model_definition):
+    with open(output_file, "w", encoding="utf8") as json_output:
+        print(json.dumps(model_definition, default=vars))
+        # print(json.dumps(model_definition.__dict__, indent=2), file=json_output)
+
 
 
 # MITRES's CWE csv export does not include parent info, just children info. Not sure about the xml yet..
@@ -89,15 +56,42 @@ def main():
     )
     parser.add_argument('-f', '--format', help='input format type [xml, csv]', choices=FUNCTION_MAP.keys())
     parser.add_argument('-i', '--input_file', help='input filename, absolute or relative filepath')
+    parser.add_argument('-m', '--model_name', help='name of the model')
     parser.add_argument('-o', '--output', help='output filename, extension will be generated')
     parser.add_argument('-v', '--version')
     args = parser.parse_args()
     process = FUNCTION_MAP[args.format]
     tree = process(args.input_file)
+    model_definition = JSONRoot(args.model_name, {}, {}, {}, tree.values(), {})
+    print(type(model_definition.measures))
+    exit(0)
+    export_to_json(args.output, model_definition)
 
-    for id in tree.keys():
-        if len(tree[id].children_ids) == 0:
-            print(id)
+
+class JSONRoot:
+    def __init__(self, name, additional_data, global_config, factors, measures, diagnostics):
+        self.name = name
+        self.additionalData = additional_data
+        self.global_config = global_config
+        self.factors = factors
+        self.measures = measures
+        self.diagnostics = diagnostics
+
+    def __str__(self):
+        return self.name
+
+
+class MeasureNode:
+    def __init__(self, cwe_id, name, weakness_abstraction, description, children):
+        self.cwe_id = cwe_id
+        self.name = name
+        self.positive = "false"
+        self.weakness_abstraction = weakness_abstraction
+        self.description = description
+        self.children = children
+
+    def __str__(self):
+        return self.name + " Measure Node"
 
 
 if __name__ == "__main__":

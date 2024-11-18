682f496e5a8a907086a38f73ab3372f254d4811c
Derek Reimanis <derek.reimanis@gmail.com>
2024-11-18 14:57:44 -0700

    fixed css id for dockerhub scrape

--- cloud-benchmark-generator/main.py
+++ cloud-benchmark-generator/main.py
@@ -6,39 +6,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 projects = [
-    "alpine",
-    "nginx",
-    "busybox",
-    "ubuntu",
-    "python",
-    "redis",
-    "postgres",
-    "memcached",
-    "node",
-    "httpd",
-    "mongo",
-    "mysql",
-    "traefik",
-    "rabbitmq",
-    "docker",
-    "mariadb",
-    "hello-world",
-    "openjdk",
-    "golang",
-    "registry",
-    "wordpress",
-    "debian",
-    "centos",
-    "php",
-    "consul"
-
+    "nginx"
 ]
 
 
 def export_to_json(master_map):
     if not os.path.exists("out/"):
         os.makedirs("out/")
-    with open('out/docker-projects-'+str(len(projects))+'-benchmark.json', "w") as outfile:
+    with open('out/docker-projects-'+ projects[0] + '-benchmark.json', "w") as outfile:
         json.dump(master_map, outfile, indent=2)
 
 
@@ -50,15 +25,15 @@ def main():
         project_versions = []
         webpage = "https://hub.docker.com/_/" + project + "/tags"
         driver.get(webpage)
-        time.sleep(3)
+        time.sleep(6)
         #page counter, start at 2 because page 1 is the default tags page
         page_increment = 2;
         while True:
             # base case, this is a bit of a do-while loop
-            final_page_elements = driver.find_elements(By.XPATH, "//div[@class='MuiTypography-root MuiTypography-subtitle1 css-1dufp5b']")
+            final_page_elements = driver.find_elements(By.XPATH, "//div[@class='MuiTypography-root MuiTypography-subtitle1 css-13vrtj1']")
             if len(final_page_elements) > 0 and final_page_elements[0].text == "Tags not retrieved":
                 break
-            versions = driver.find_elements(By.XPATH, "//a[@class='MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1nqtld6' and @data-testid='navToImage']")
+            versions = driver.find_elements(By.XPATH, "//a[@class='MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-162qm3z' and @data-testid='navToImage']")
             for element in versions:
                 # ignore latest, because latest is a duplicate of the one after latest
                 if element.text != 'latest':
